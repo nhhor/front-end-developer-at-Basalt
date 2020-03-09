80a39c374639aabf50fc2b81007f77e9d9e8d625
nhhor <nhhor@post.com>
2020-03-08 20:43:03 -0700

    Updates

---
+++ precache-manifest.938307bfcaffba3d118bfd2fdd09c0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3814db4a2b40c57824de66651abef703",
+    "url": "/front-end-developer-at-Basalt/index.html"
+  },
+  {
+    "revision": "f98786fd4cde97c0fdd4",
+    "url": "/front-end-developer-at-Basalt/static/css/main.da62f3f3.chunk.css"
+  },
+  {
+    "revision": "cb9d7fe76dadd77819e8",
+    "url": "/front-end-developer-at-Basalt/static/js/2.65c89037.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/front-end-developer-at-Basalt/static/js/2.65c89037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98786fd4cde97c0fdd4",
+    "url": "/front-end-developer-at-Basalt/static/js/main.c7ac1b92.chunk.js"
+  },
+  {
+    "revision": "f30ed5b4e8fa1c91cc3d",
+    "url": "/front-end-developer-at-Basalt/static/js/runtime-main.42efe6aa.js"
+  }
+]);
